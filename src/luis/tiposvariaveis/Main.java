90ecbbfb7f7e9d814d714e209adfc3ac11f489a0
Luis Miguel <luiskasami@gmail.com>
2025-08-25 22:33:26 -0300

    Novas atividades feitas de operadores e seus exemplos

---
+++ src/luis/tiposvariaveis/Main.java
@@ -0,0 +1,17 @@
+package luis.tiposvariaveis;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double salarioMinimo = 2.500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+         int numero = 1;
+        numero = 3;
+        System.out.println(numero);
+
+    }
+}
